c14414677c2469add6731ead9d9c18170fb969ca
Subrina Sirajee <73296278+Subrina-Sirajee@users.noreply.github.com>
2025-05-14 21:45:30 +0600

    Update prompt_ui.py

--- prompt_ui.py
+++ prompt_ui.py
@@ -1,4 +1,4 @@
-from langchain_openai import ChatOpenAI
+from langchain.chat_models import ChatOpenAI
 from dotenv import load_dotenv
 import streamlit as st
 from langchain_core.prompts import PromptTemplate,load_prompt
